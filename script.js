144cfbc7ed3a98caab8cc2fa2b55964aec59e3b4
rolincarrizo <rolincarrizo26@gmail.com>
2024-12-23 07:58:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("Bienvenidos");
+var edad = prompt("¿Cual es tu edad?");
+if(edad >= 18){
+    alert("Eres mayor de Edad");
+}else{
+    alert("Eres menor de Edad");
+}
+var nota = prompt ("Introduce tu nota")
+if(nota >6){
+alert("aprobado");
+}else {
+alert("No aprobó")
+}
